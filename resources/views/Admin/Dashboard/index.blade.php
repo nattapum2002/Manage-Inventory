da501fb518d3c41f4fe6b0d426070e5624d99c90
nattapum <nattapum.kh@rmuti.ac.th>
2024-11-05 15:35:57 +0700

    commit

---
+++ resources/views/Admin/Dashboard/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.master')
+
+@section('title')
+    Dashboard
+@endsection
+
+@section('content')
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+
+            </div>
+        </div>
+    </section>
+@endsection
