e32de041f92bd32a281d6dadfd43daf9df9a3bf6
nattapum <nattapum.kh@rmuti.ac.th>
2024-11-05 14:52:08 +0700

    commit

--- resources/views/layouts/css.blade.php
+++ resources/views/layouts/css.blade.php
@@ -1,2 +1,4 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.dataTables.css" />
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,14 +1,70 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ระบบจัดการคลังสินค้า</title>
 </head>
-    @include('layouts.css')
+@include('layouts.css')
+
 <body>
-    
+
+    <div class="wrapper">
+        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i
+                            class="fas fa-bars"></i></a>
+                </li>
+                <li class="nav-item d-none d-sm-inline-block">
+                    {{-- <a href="{{ route('home') }}" class="nav-link">Home</a> --}}
+                </li>
+            </ul>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item dropdown user-menu">
+                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
+                        {{-- <img src="{{ asset('backend/dist/img/AdminLTELogo.png') }}"
+                            class="user-image img-circle elevation-2" alt="User Image"> --}}
+                        <span class="d-none d-md-inline"></span>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                        <!-- User image -->
+                        <li class="user-header bg-primary">
+                            {{-- <img src="{{ asset('backend/dist/img/AdminLTELogo.png') }}" class="img-circle elevation-2"
+                                alt="User Image"> --}}
+                        </li>
+                        <!-- Menu Footer-->
+                        <li class="user-footer">
+                            {{-- <a href="#" class="btn btn-default btn-flat float-right"
+                                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                Sign out
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form> --}}
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+
+        @include('layouts.sidebar')
+
+        <div class="content-wrapper">
+            @yield('content')
+        </div>
+
+        <footer class="main-footer">
+            <div class="float-right d-none d-sm-block">
+                <b>Version</b> 1.0
+            </div>
+            <strong>Manage Inventory System (MIS)</strong>
+        </footer>
+    </div>
+
     @include('layouts.js')
 </body>
+
 </html>
