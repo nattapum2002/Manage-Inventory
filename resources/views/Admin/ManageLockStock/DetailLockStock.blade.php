3f212c5767c58d3710a7755ce59324b100879954
aumaphitep <aumaphitep@gmail.com>
2024-12-11 15:45:59 +0700

    fix

--- resources/views/Admin/ManageLockStock/DetailLockStock.blade.php
+++ resources/views/Admin/ManageLockStock/DetailLockStock.blade.php
@@ -6,12 +6,12 @@
 
 @section('content')
     <section class="content">
-        @if (session('success'))
+        {{-- @if (session('success'))
             <div class="alert alert-success alert-dismissible fade show" role="alert">
                 {{ session('success') }}
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
-        @endif
+        @endif --}}
         <div class="container-fluid">
             <div class="row">
                 <div class="col-12">
